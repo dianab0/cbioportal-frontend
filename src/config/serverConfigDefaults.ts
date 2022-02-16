e3a89e87a8d9081d06c931d4e33604f3a92feae9
Diana Baiceanu <diana@thehyve.nl>
2022-02-16 20:32:09 +0100

    Added new oncoprint property for default tracks
    Clinical tracks can be configured to show by default using a JSON
    configuration file saved into classpath:
    oncoprint.clinical_tracks.show_by_default =classpath:/oncoprint-default-tracks.json
    
    Example of file:
    [{"stableId": "CANCER_TYPE", "sortOrder": "ASC", "gapOn": true},
     {"stableId": "MUTATION_COUNT", "sortOrder": "DESC", "gapOn": false},
     {"stableId": "AGE", "sortOrder": null, "gapOn": null}]
    
    Commit includes review comments and e2e test as well

--- src/config/serverConfigDefaults.ts
+++ src/config/serverConfigDefaults.ts
@@ -195,6 +195,8 @@ const ServerConfigDefaults: Partial<IServerConfig> = {
     skin_home_page_unauthorized_studies_global_message:
         'The study is unauthorized. You need to request access.',
     comparison_categorical_na_values: 'NA',
+
+    oncoprint_clinical_tracks_show_by_default: '',
 };
 
 export default ServerConfigDefaults;
